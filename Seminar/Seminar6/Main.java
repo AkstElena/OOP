c1b6746114affde58687ff525993c28a274af619
Akst Elena <akstelena@yandex.ru>
2023-08-17 15:11:09 +0300

    Seminar 6 and Homework 6

---
+++ Seminar/Seminar6/Main.java
@@ -0,0 +1,11 @@
+package Seminar.Seminar6;
+
+import Seminar.Seminar6.view.UserView;
+
+public class Main {
+  public static void main(String[] args) {
+    UserView usrView = new UserView();
+    usrView.startInterface();
+
+  }
+}
